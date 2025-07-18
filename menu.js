18a7ea537611538aaca7a2601fc371705f071152
David Laurentino <dev.laurentino@gmail.com>
2025-07-18 17:54:33 -0300

    Atualizando CSS, atualizando numero e deixando o site responsivo

--- menu.js
+++ menu.js
@@ -1,4 +1,13 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', this.window.scrollY > 100)
-})
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('#header');
+  const toggle = document.getElementById('menu-toggle');
+  const nav = document.querySelector('nav');
+
+  window.addEventListener('scroll', function () {
+    header.classList.toggle('rolagem', window.scrollY > 100);
+  });
+
+  toggle.addEventListener('click', function () {
+    nav.classList.toggle('ativo');
+  });
+});
